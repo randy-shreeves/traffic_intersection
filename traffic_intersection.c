3905109fa49ac5dff07ad1a66cca8d419819fec2
Randy Shreeves <114593302+RandyShreeves@users.noreply.github.com>
2023-11-08 20:24:28 -0500

    Define macros for ports and clock
    Defined macros for names and addresses of the registers for the output
    ports B and F as well as input port E. Also defined macro for the
    address of the clock gating control.

--- traffic_intersection.c
+++ traffic_intersection.c
@@ -10,6 +10,32 @@
 // Date: 11/08/2023
 
 // 1. Pre-processor Directives Section
+// Define Port B adresses
+#define GPIO_PORTB_DATA_R (*((volatile unsigned long *)0x400053FC))
+#define GPIO_PORTB_DIR_R (*((volatile unsigned long *)0x40005400))
+#define GPIO_PORTB_AFSEL_R (*((volatile unsigned long *)0x40005420))
+#define GPIO_PORTB_DEN_R (*((volatile unsigned long *)0x4000551C))
+#define GPIO_PORTB_AMSEL_R (*((volatile unsigned long *)0x40005528))
+#define GPIO_PORTB_PCTL_R (*((volatile unsigned long *)0x4000552C))
+
+// Define Port E addresses
+#define GPIO_PORTE_DATA_R (*((volatile unsigned long *)0x400243FC))
+#define GPIO_PORTE_DIR_R (*((volatile unsigned long *)0x40024400))
+#define GPIO_PORTE_AFSEL_R (*((volatile unsigned long *)0x40024420))
+#define GPIO_PORTE_DEN_R (*((volatile unsigned long *)0x4002451C))
+#define GPIO_PORTE_AMSEL_R (*((volatile unsigned long *)0x40024528))
+#define GPIO_PORTE_PCTL_R (*((volatile unsigned long *)0x4002452C))
+	
+// Define Port F addresses
+#define GPIO_PORTF_DATA_R (*((volatile unsigned long *)0x400253FC))
+#define GPIO_PORTF_DIR_R (*((volatile unsigned long *)0x40025400))
+#define GPIO_PORTF_AFSEL_R (*((volatile unsigned long *)0x40025420))
+#define GPIO_PORTF_DEN_R (*((volatile unsigned long *)0x4002551C))
+#define GPIO_PORTF_AMSEL_R (*((volatile unsigned long *)0x40025528))
+#define GPIO_PORTF_PCTL_R (*((volatile unsigned long *)0x4002552C))
+	
+// Define Clock Gating Control address
+#define SYSCTL_RCGC2_R (*((volatile unsigned long *)0x400FE108))
 
 // 2. Global Declarations Section
 
